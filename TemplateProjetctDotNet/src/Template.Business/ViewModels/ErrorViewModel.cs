61de9dcb16deba67147df8c21176642365829872
Fabiana Angelo <fabiana.lima@wdatecnologia.com.br>
2022-10-08 11:45:13 -0300

    Criacao das ViewModels

---
+++ TemplateProjetctDotNet/src/Template.Business/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Business.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
